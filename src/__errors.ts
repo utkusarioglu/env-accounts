3f748220b32a537b85eb6b78e735be72ab262d6e
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-11-12 11:53:03 +0000

    Add EnvParser errors

--- src/__errors.ts
+++ src/__errors.ts
@@ -1,6 +1,8 @@
 const ERROR_STRINGS_EN = {
-  NO_EMPTY_STRING: 'EnvPrefix cannot be an empty string',
+  NO_EMPTY_STRING: "EnvPrefix cannot be an empty string",
   NO_GET_WITHOUT_SET: "EnvPrefix hasn't been se",
+  SOURCE_SHALL_BE_OBJECT: "Source has to be an object",
+  NO_CALL_BEFORE_PARSE: "Cannot call before parse",
 };
 
 export default ERROR_STRINGS_EN;
